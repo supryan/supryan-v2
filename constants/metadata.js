eb75a80fdddea99a23cf0064abdd66884347cbd9
Ryan Gordon <ryan.gordon@useallfive.com>
2022-03-25 16:05:09 -0700

    Metadata

--- constants/metadata.js
+++ constants/metadata.js
@@ -1,10 +1,10 @@
 // TODO These defaults are overwritten in Prismic via Singleton Metadata or Template-specific overrides
 const metaDefaults = {
-  TWITTER_HANDLE: '{@twitterHandle}',
-  TITLE: '{Default Title}',
-  DESCRIPTION: '{Default description (50 - 155 characters)}',
+  TWITTER_HANDLE: '@supryan',
+  TITLE: `Sup. I'm Ryan Gordon.`,
+  DESCRIPTION: 'Creative Technologist. I make things for the Internet. ✌️',
   DIVIDER: '|',
-  IMAGE: '/images/share.jpg', // TODO should be 1200 x 627px
+  IMAGE: '/images/share.png',
 }
 
 export default metaDefaults
