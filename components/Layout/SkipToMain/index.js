20f096de0fd5278e1e44bccd7efec051c20124f9
Ryan Gordon <ryan.gordon@useallfive.com>
2022-12-29 15:28:12 -0800

    Skip to Main

---
+++ components/Layout/SkipToMain/index.js
@@ -0,0 +1,11 @@
+import styles from './index.module.scss'
+
+const SkipToMain = () => {
+  return (
+    <a className={styles.skipToMain} href="#projects">
+      Skip to content
+    </a>
+  )
+}
+
+export default SkipToMain
