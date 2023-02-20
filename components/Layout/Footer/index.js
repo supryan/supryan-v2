834a6fd169585ec55eb0672fad2983bcfa90147a
Ryan Gordon <ryan.gordon@useallfive.com>
2023-02-20 09:10:02 -0800

    Social and type fixes

--- components/Layout/Footer/index.js
+++ components/Layout/Footer/index.js
@@ -12,7 +12,7 @@ export default function Footer({ footer }) {
             <Logo className={styles.logo} />
             <sup className={styles.links}>
               <PrismicLink link={footer?.github}>Github</PrismicLink>
-              <PrismicLink link={footer?.twitter}>Twitter</PrismicLink>
+              <PrismicLink link={footer?.medium}>Medium</PrismicLink>
               <PrismicLink link={footer?.email}>Email</PrismicLink>
             </sup>
           </sup>
